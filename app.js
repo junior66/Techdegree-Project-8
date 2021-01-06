75382dd94f56b837701c628504cd00cd1a54ca97
junior66 <jraidee@gmail.com>
2021-01-06 01:10:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -66,3 +66,15 @@ fetch(urlAPI)
         modalContainer.innerHTML = modalHTML;
 
    }
+
+   gridContainer.addEventListener(click, e => {
+    //    make sure the click is not on the gridContainer itself
+    if(e.target !== gridContainer) {
+        // select the card element based on its proximity to actual element clicked
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index');
+        
+        displayModal(index);
+    }
+
+});
