ffc6781039ef146dfb6ff7baca25afb70bfccab1
junior66 <jraidee@gmail.com>
2021-01-05 22:39:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -43,3 +43,26 @@ fetch(urlAPI)
         `
         gridContainer.innerHTML = employeeHTML; 
     });
+
+   function displayModal(index) {
+    //    use object destructuring to make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}"/>
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            </hr>
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday:
+            ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}
+            </p>
+        </div>
+    `;
+        overlay.classList.remove("hidden");
+        modalContainer.innerHTML = modalHTML;
+
+   }
